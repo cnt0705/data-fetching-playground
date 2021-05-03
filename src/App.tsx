154a1f475ed4c97794b770e8565dd561670c15c6
Chinatsu Kato <cnt.tkhs@gmail.com>
2021-05-03 18:28:48 +0900

    Basic usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
 import React from 'react'
+import useSWR from 'swr'
 
-export const App = () => <p>Template</p>
+export const App = () => {
+  const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+  const { data, error } = useSWR(
+    'https://jsonplaceholder.typicode.com/todos/2',
+    fetcher
+  )
+
+  if (error) {
+    throw error
+  }
+
+  if (!data) return <div>Loading...</div>
+
+  console.log(data)
+
+  return (
+    <div>
+      <h1>Fetching data with SWR</h1>
+    </div>
+  )
+}
